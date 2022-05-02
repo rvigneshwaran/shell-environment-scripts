3b8038ed55fa34e9bc1b73e067dc4d05f2f425c5
Vigneshwaran Rethenasekaran <redhatvicky@gmail.com>
2022-05-02 10:30:52 +0530

    Script for play with variables

--- 02-inbuild-execution.sh
+++ 02-inbuild-execution.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+currentdate=$(date)
+echo "Current date and time :: $currentdate"
 
+current_directory=$(pwd)
+echo "The Current working directory :: $current_directory"
+
+files_list=$(ls)
+echo "File List in the current directory  :: $files_list"
